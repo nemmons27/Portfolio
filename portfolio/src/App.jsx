36cc8dc951cc9bfe21eea9d11d92122be23aff3d
Nicholas Emmons <emmonsnicholas7@gmail.com>
2023-07-27 13:46:19 -0400

    Components created and App opened up

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  const getYear = () => {
+    return new Date().getFullYear()
+  }
+
 
   return (
     <div>
+    <header>
+      <Nav />
+    </header>
 
+    <br />
+
+    <main>
+      <div>
+
+      </div>
+      <br />
+      <footer className='footer'>
+      © {getYear()} Emmons, all rights reserved
+      </footer>
+    </main>
     </div>
   )
 }
