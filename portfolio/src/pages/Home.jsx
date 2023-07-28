b8916c45feebc8207f98e646b50b36b27c300f99
Nicholas Emmons <emmonsnicholas7@gmail.com>
2023-07-28 19:09:18 -0400

    Home Page filled out

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+
+    return (
+        <div>
+            <h3 className="header">Nicholas Emmons</h3>
+            <h5 className="subheader">Software Engineer - Anime Maniac - Fullstack Developer</h5>
+        </div>
+    )
+}
+
+export default Home
