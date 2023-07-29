b147583d2116b2273c05ef3d0f7f5c356553a705
Nicholas Emmons <emmonsnicholas7@gmail.com>
2023-07-28 20:35:36 -0400

    Css edits showing

--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -3,12 +3,11 @@ import React from "react";
 const About = () => {
     return (
         <div>
-            <h3>Hey, I'm Nicholas.</h3>
-            <h5>Software Engineer - Anime Maniac - Fullstack Developer</h5>
+            <h3 className="subheader">Hey, I'm Nicholas.</h3>
             <br />
             <img src="https://avatars.githubusercontent.com/u/131407670?v=4" className="myProfile" alt="myProfile" />
             <br />
-            <a href="https://www.linkedin.com/in/nemmons27/">LinkedIn</a> | <a href="https://github.com/nemmons27">Github</a>
+            <a href="https://www.linkedin.com/in/nemmons27/" className="linkedinPlug">LinkedIn</a> | <a href="https://github.com/nemmons27" className="githubPlug">Github</a>
             <br />
             <p>I'm a Software Engineer with nothing but potential! This site will be the proof of it.</p>
             <p></p>
