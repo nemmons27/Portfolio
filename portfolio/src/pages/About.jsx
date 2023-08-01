2030684d98cc907c03b1da4bfab0568c9fc3be20
Nicholas Emmons <emmonsnicholas7@gmail.com>
2023-07-31 20:24:25 -0400

    Cards for Projects created

--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -1,27 +1,20 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const About = () => {
 
-    const handleClick = () => {
-        fetch(('public/Nicholas Emmons - Resume.pdf').then(res => {
-            res.blob().then(blob => {
-                const fileUrl = window.URL.createObjectURL(blob);
-                let alink = document.createElement('a');
-                alink.href = fileUrl;
-                alink.download = 'public/Nicholas Emmons - Resume.pdf';
-                alink.click()
-            })
-        }))
+    const downloadFile = () => {
+        window.location.href = "https://localhost:5173/src/assets/Emmons_Resume.pdf"
     }
-
-
     return (
         <div>
             <h3 className="subheader">Hey, I'm Nicholas.</h3>
             <br />
             <img src="https://avatars.githubusercontent.com/u/131407670?v=4" className="myProfile" alt="myProfile" />
             <br />
-            <a href="https://www.linkedin.com/in/nemmons27/" className="linkedinPlug">LinkedIn</a> | <a href="https://github.com/nemmons27" className="githubPlug">Github</a> | <button onClick={handleClick} className="resumeBtn" target="_blank">Resumé</button>
+            <a href="https://www.linkedin.com/in/nemmons27/" className="linkedinPlug">LinkedIn</a> | <a href="https://github.com/nemmons27" className="githubPlug">Github</a> |
+            <a href="/src/assets/Emmons_Resume.pdf" download="Emmons_Resume.pdf"target="_blank">
+                <button onClick={downloadFile} className="resumeBtn">Resumé</button>
+            </a>
             <br />
             <p>I'm a Software Engineer with nothing but potential! This site will be the proof of it.</p>
             <p></p>
--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -6,6 +6,16 @@ const Home = () => {
         <div>
             <h3 className="header">Nicholas Emmons</h3>
             <h5 className="subheader">Software Engineer - Anime Maniac - Fullstack Developer</h5>
+            <br />
+            <h5 className="subheader">Web Applications</h5>
+            <div className="card">
+                <Link to="https://clearance-b56453d7410f.herokuapp.com/" target="_blank">
+                    <img src="" alt="" />
+                    <h3>Clearance</h3>
+                </Link>
+            </div>
+            <br />
+            <h5 className="subheader">Browser-Based Games</h5>
         </div>
     )
 }
